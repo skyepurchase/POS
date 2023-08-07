cef363fa3de36308b7231535d333e251f4d059c9
Skye Purchase <atp45@cam.ac.uk>
2023-08-07 17:47:59 +0100

    Adding file checking

--- src/file_handler.c
+++ src/file_handler.c
@@ -3,18 +3,22 @@
 
 char *file_to_string(char *filename) {
     FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Could not open %s to convert to string", filename);
+        return NULL;
+    }
+
     char *buffer = 0;
     long length;
     
-    if (file != NULL) {
-        fseek(file, 0, SEEK_END);
-        length = ftell(file);
-        fseek(file, 0, SEEK_SET);
-        
-        buffer = (char *)malloc(length);
-        if (buffer != NULL) {
-            fread(buffer, 1, length, file);
-        }
+    fseek(file, 0, SEEK_END);
+    length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    
+    buffer = (char *)malloc(length);
+    if (buffer != NULL) {
+        fread(buffer, 1, length, file);
     }
 
     return buffer;
