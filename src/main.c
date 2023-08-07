4f1fd09879dbfc2076dd3685e82e304320f370dc
Skye Purchase <atp45@cam.ac.uk>
2023-08-07 17:59:15 +0100

    Using new file handler to open file

--- src/main.c
+++ src/main.c
@@ -2,33 +2,39 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include "file_handler.h"
+
 int main(int argc, char *argv[]) {
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
     }
 
-    FILE *file_to_read = fopen(argv[1], "r");
+    char *program = file_to_string(argv[1]);
+
+    if (program == NULL) {
+        printf("Couldn't load program from %s.", argv[1]);
+    }
+
+    printf("%s", program);
+
     FILE *file_to_write = fopen(argv[2], "w");
 
-    if (file_to_read == NULL) {
-        printf("%s file could not be opened!\n", argv[1]);
-        return -1;
-    }
     if (file_to_write == NULL) {
         printf("Output file could not be opened!\n");
         return -1;
     }
 
-    char c;
-    while ((c = fgetc(file_to_read)) != EOF) {
+    int size = 10;
+    int id;
+    for (id = 0; id < size; ++id) {
+        char c = program[id];
         if (c == '+') {
             c = '-';
         }
         fputc(c, file_to_write);
     }
 
-    fclose(file_to_read);
     fclose(file_to_write);
 
     return 0;
