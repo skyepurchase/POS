200e5ecdeecaf19963d04b8131174e6a27f106e2
Skye Purchase <atp45@cam.ac.uk>
2023-08-07 16:01:26 +0100

    Basic file editor

--- src/main.c
+++ src/main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
-#include "hello_world.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-int main() {
-    hello_world();
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
+        return 1;
+    }
+
+    FILE *file_to_read = fopen(argv[1], "r");
+    FILE *file_to_write = fopen(argv[2], "w");
+
+    if (file_to_read == NULL) {
+        printf("%s file could not be opened!\n", argv[1]);
+        return -1;
+    }
+    if (file_to_write == NULL) {
+        printf("Output file could not be opened!\n");
+        return -1;
+    }
+
+    char c;
+    while ((c = fgetc(file_to_read)) != EOF) {
+        if (c == '+') {
+            c = '-';
+        }
+        fputc(c, file_to_write);
+    }
+
+    fclose(file_to_read);
+    fclose(file_to_write);
 
     return 0;
 }
